806a8b747a57e6bcb39053f452a60c0419d88006
berndmildt <bernd.mildt@smail.th-koeln.de>
2025-03-31 08:56:48 +0200

    Changed code for ruff disturbance error

--- course_source.py
+++ course_source.py
@@ -1,9 +1,9 @@
 
 
 # Function 1: Train Cargo capacities from Austria (AT) to Poland (PL) per year
-european_train_routes = 35000  # Total number of train routes in Europe
-european_train_cargo_capacity_per_route = 300  # in tonnes (average capacity of each route)
-relative_share_AT_PL = 0.01 # AT to PL train routes relative to whole EU train routes
+european_train_routes =                                 35000  # Total number of train routes in Europe
+european_train_cargo_capacity_per_route         = 300  # in tonnes (average capacity of each route)
+relative_share_AT_PL=0.01# AT to PL train routes relative to whole EU train routes
 
 def get_train_routes_and_capacities(european_train_routes, european_train_cargo_capacity_per_route, relative_share_AT_PL):
     total_cargo_capacity_AT_to_PL = european_train_routes * european_train_cargo_capacity_per_route * relative_share_AT_PL
@@ -14,7 +14,7 @@ print(f"Annual total Cargo Capacity from AT to PL: {total_cargo_capacity_solutio
 
 
 # Function 2: Calculate how much Photovoltaik module scrap could be transported per year from Austria to Poland
-photovoltaik_scrap_generated_annual_AT = 200000 # tonnes
+photovoltaik_scrap_generated_annual_AT= 200000 # tonnes
 transport_factor = 0.7
 
 def calculate_photovoltaik_scrap_transport_per_year(photovoltaik_scrap_generated_annual_AT, transport_factor):
@@ -36,8 +36,8 @@ total_scrap_transport_solution = calculate_photovoltaik_scrap_transport_2030_205
 print(f"Total scrap transport from AT to PL 2030 to 2050: {total_scrap_transport_solution}")
 
 # Function 4: To check if enough train cargo capacity for transportation exists
-def enough_train_cargo_capacity(scrap_transportable_from_AT_to_PL_solution, total_cargo_capacity_solution):
-    enough = scrap_transportable_from_AT_to_PL_solution / total_cargo_capacity_solution
+def enough_train_cargo_capacity(             scrap_transportable_from_AT_to_PL_solution       , total_cargo_capacity_solution      )           :
+    enough      = scrap_transportable_from_AT_to_PL_solution /      total_cargo_capacity_solution
     return 0
 
 enough_solution = enough_train_cargo_capacity(140000, 105000)
